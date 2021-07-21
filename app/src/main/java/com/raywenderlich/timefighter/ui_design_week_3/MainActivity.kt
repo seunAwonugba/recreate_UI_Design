0be1f082a2106b1d377c0c521246ccac4f8c970f
seunawonugba <seun.awonugba@decagon.dev>
2021-07-21 13:41:32 +0100

    second commit

--- app/src/main/java/com/raywenderlich/timefighter/ui_design_week_3/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/ui_design_week_3/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.raywenderlich.timefighter.ui_design_week_3
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * Create an instance of the scroll view
+     */
+
+    private lateinit var switchInstance : SwitchCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * Grab switchInstance by its ID
+         */
+        switchInstance=findViewById(R.id.switchId)
+        switchInstance.setOnClickListener { darkMode() }
+
+
+    }
+
+    private fun darkMode(){
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES ->
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            Configuration.UI_MODE_NIGHT_NO ->
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
     }
 }
